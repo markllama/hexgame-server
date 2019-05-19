debcf7967bea26e189c8f3bcbf29f150b78c23ff
Mark Lamourine <markllama@gmail.com>
2019-05-19 16:56:48 -0400

    simple test of add

--- hexmap/vector.go
+++ hexmap/vector.go
@@ -1,19 +1,30 @@
 // hexmap/vector - a representation of a single location on a hex map
 package hexmap
 
-//import (
+import (
+	"fmt"
 //	"encoding/json"
-//)
+)
 
 type Vector struct {
 	Hx int `json:"hx"`
 	Hy int `json:"hy"`
 }
 
+func (v Vector) String() string {
+	return fmt.Sprintf("Vector(hx: %d, hy: %d)", v.Hx, v.Hy)
+}
+
+// Hz is dependent on Hx and Hy
 func (v Vector) Hz() int {
 	return v.Hy - v.Hx
 }
 
+// The length of a HexVector is the absolute value of the largest component
 func (v Vector) Length() int {
 	return max(abs(v.Hx), abs(v.Hy), abs(v.Hz()))
+}
+
+func (v0 Vector) Add(v1 Vector) Vector {
+	return Vector{v0.Hx + v1.Hx, v0.Hy + v1.Hy}
 }
--- hexmap/vector_test.go
+++ hexmap/vector_test.go
@@ -20,4 +20,36 @@ func TestLength(t *testing.T) {
 	if hv0.Length() != 0 {
 		t.Error()
 	}
+
+	hv1 := Vector{1, 1}
+	if hv1.Length() != 1 {
+		t.Error()
+	}
+	
+	hv2 := Vector{-1, -1}
+	if hv2.Length() != 1 {
+		t.Error()
+	}
+	
+	hv3 := Vector{0, -1}
+	if hv3.Length() != 1 {
+		t.Error()
+	}
+}
+
+func TestAdd(t *testing.T) {
+	hv0 := Vector{5, 6}
+	hv1 := Vector{5, 5}
+
+	result0 := hv0.Add(ORIGIN)
+
+	if result0 != hv0 {
+		t.Error()
+	}
+
+	result1 := hv0.Add(UNIT[0])
+
+	if result1 != hv1 {
+		t.Errorf("hv0 %s != result1 %s", hv1, result1)
+	}
 }
