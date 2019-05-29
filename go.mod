f8d25591fcf230b9806669ae4f8b7a1eaca88b21
Mark Lamourine <markllama@gmail.com>
2019-05-28 21:31:00 -0400

    moved handlers up one level

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/snikch/goodman v0.0.0-20171125024755-10e37e294daa // indirect
 	github.com/unrolled/render v1.0.0
 	github.com/urfave/negroni v1.0.0
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
---
+++ service/map_handler_test.go
@@ -0,0 +1,51 @@
+package handlers
+//
+//
+//
+import (
+	//	"github.com/unrolled/render"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/markllama/hexgame-server/service"
+	"github.com/markllama/hexgame-server/hexmap"
+)
+
+// var (
+// 	formatter = render.New(render.Options{ IndentJSON: true })
+// )
+
+func TestGetMapList(t *testing.T) {
+	var (
+		request *http.Request
+		recorder *httptest.ResponseRecorder
+	)
+
+	server := service.NewServer()
+
+	recorder = httptest.NewRecorder()
+	request, _ = http.NewRequest("GET", "/maps/", nil)
+	server.ServeHTTP(recorder, request)
+
+	allMaps := []hexmap.Map {
+		
+	}
+
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected %v; received %v", http.StatusOK, recorder.Code)
+	}
+	payload, err := ioutil.ReadAll(recorder.Body)
+	if err != nil {
+		t.Errorf("Error parsing response body: %v", err)
+	}
+	err = json.Unmarshal(payload, &allMaps)
+	if err != nil {
+		t.Errorf("Error unmarshaling response to a list of maps: %v", err)
+	}
+}
+	
+
+
