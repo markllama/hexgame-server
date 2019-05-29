88cea658ba0af4161690cb5e12e60e25ccfd7432
Mark Lamourine <markllama@gmail.com>
2019-05-28 23:10:53 -0400

    test return of id of a single map

--- service/map_handler.go
+++ service/map_handler.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"net/http"
+	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
 	"github.com/markllama/hexgame-server/hexmap"
 )
@@ -15,6 +16,9 @@ func mapListHandler(formatter *render.Render) http.HandlerFunc {
 
 func mapHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w, http.StatusOK, hexmap.Map {} )
+		vars := mux.Vars(req)
+		mapId := vars["id"]
+		
+		formatter.JSON(w, http.StatusOK, hexmap.Map { Name: mapId } )
 	}	
 }
--- service/map_handler_test.go
+++ service/map_handler_test.go
@@ -46,5 +46,37 @@ func TestGetMapList(t *testing.T) {
 	}
 }
 	
+func TestGetMapL(t *testing.T) {
+	var (
+		request *http.Request
+		recorder *httptest.ResponseRecorder
+	)
+
+	server := NewServer()
+
+	recorder = httptest.NewRecorder()
+	request, _ = http.NewRequest("GET", "/maps/12345", nil)
+	server.ServeHTTP(recorder, request)
+
+	aMap := hexmap.Map {
+		
+	}
+
+	if recorder.Code != http.StatusOK {
+		t.Errorf("Expected %v; received %v", http.StatusOK, recorder.Code)
+	}
+	payload, err := ioutil.ReadAll(recorder.Body)
+	if err != nil {
+		t.Errorf("Error parsing response body: %v", err)
+	}
+	err = json.Unmarshal(payload, &aMap)
+	if err != nil {
+		t.Errorf("Error unmarshaling response to a request for a map: %v", err)
+	}
+
+	if aMap.Name != "12345" {
+		t.Errorf("Wrong ID: %s: %v", aMap.Name, err)		
+	}
+}
 
 
